055b02b430f10026aac6692e55e23e81a943c041
Roshan Khadka <khadkaasd963@gmail.com>
2026-01-02 11:18:43 +0545

    creating auth state for viewmodel

--- lib/features/auth/domain/usecases/login_usecase.dart
+++ lib/features/auth/domain/usecases/login_usecase.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:fanup/core/error/failures.dart';
 import 'package:fanup/core/usecases/app_usecase.dart';
 import 'package:fanup/features/auth/data/repositories/auth_repository.dart';
-import 'package:fanup/features/auth/domain/entities/auth_entity.dart';
+
 import 'package:fanup/features/auth/domain/repositories/auth.repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -10,24 +10,20 @@ class LoginUsecaseParams {
   final String email;
   final String password;
 
-  const LoginUsecaseParams({
-    required this.email,
-    required this.password,
-  });
+  const LoginUsecaseParams({required this.email, required this.password});
 }
 
 //provider for login usecase
 final loginUsecaseProvider = Provider<LoginUsecase>((ref) {
-  final authRepository = ref.watch(authRepositoryProvider);
+  final authRepository = ref.read(authRepositoryProvider);
   return LoginUsecase(authRepository: authRepository);
 });
-
 
 class LoginUsecase implements UsecaseWithParams<bool, LoginUsecaseParams> {
   final IAuthRepository _authRepository;
 
   LoginUsecase({required IAuthRepository authRepository})
-      : _authRepository = authRepository;
+    : _authRepository = authRepository;
 
   @override
   Future<Either<Failure, bool>> call(LoginUsecaseParams params) {
